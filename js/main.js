d3c68d6f7cdb5ddc8fd0eab2d667628db64b3826
Dmitriy <shamonov.dima@gmail.com>
2021-06-12 18:26:26 +0300

    use vue, add filter

--- js/main.js
+++ js/main.js
@@ -1,140 +1,83 @@
 'use strict';
 
 const urlAPI = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-const btnBusket = document.getElementById('busket');
+const imgItem = 'img/photo-product.jpg';
 
-class Cart {
-    constructor(container = '.basket') {
-        this.container = container;
-        this.goods = [];
-        this._getCart()
+const app = new Vue({
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        cartUrl: '/getBasket.json',
+        products: [],
+        cart: [],
+        filterList: [],
+        imgCatalog: imgItem,
+        userSearch: '',
+        show: false
+    },
+
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+
+        addProduct(item) {
+            this.getJson(`${urlAPI}/addToBasket.json`)
+                .then(data => {
+                    if (data.result === 1) {
+                        let find = this.cart.find(el => el.id_product === item.id_product);
+                        if (find) {
+                            find.quantity++;
+                        } else {
+                            const prod = Object.assign({ quantity: 1 }, item);
+                            this.cart.push(prod);
+                        };
+                        // увеличиваем стоимость корзины
+                        this.cart.amount += item.price;
+                    }
+                });
+        },
+
+        remove(item) {
+            this.getJson(`${urlAPI}/addToBasket.json`)
+                .then(data => {
+                    if (data.result === 1) {
+                        if (item.quantity > 1) {
+                            item.quantity--;
+                        } else {
+                            this.cart.splice(this.cart.indexOf(item), 1);
+                        }
+                        // уменьшаем стоимость корзины
+                        this.cart.amount -= item.price;
+                    }
+                })
+        },
+
+        searchFilter() {
+            let regexp = new RegExp(this.userSearch, 'i');
+            this.filterList = this.products.filter(el => regexp.test(el.product_name));
+        }
+    },
+
+    mounted() {
+        this.getJson(`${urlAPI + this.catalogUrl}`)
             .then(data => {
-                this.goods = [...data.contents];
-                this.render();
+                for (let el of data) {
+                    this.products.push(el);
+                    this.filterList.push(el);
+                }
+            });
+
+        this.getJson(`${urlAPI + this.cartUrl}`)
+            .then(data => {
+                for (let item of data.contents) {
+                    this.$data.cart.push(item);
+                };
+                this.$data.cart.amount = data.amount;
             });
     }
-
-    _getCart() {
-        return fetch(`${urlAPI}/getBasket.json`)
-            .then(result => result.json())
-            .catch(error => console.log(error))
-    }
-
-    render() {
-        const block = document.querySelector('.basket');
-        for (let product of this.goods) {
-            const productObj = new CartItem(product);
-            block.insertAdjacentHTML('beforeend', productObj.render());
-        }
-    }
-
-    // добавить товар в корзину
-    addCart() {
-
-    }
-
-    // очистить всю корзину
-    cleanCart() {
-
-    }
-
-    // оплатить корзину
-    buyCart() {
-
-    }
-
-};
-
-class CartItem {
-    constructor(product, img = 'img/photo-product.jpg') {
-        this.title = product.product_name;
-        this.price = product.price;
-        this.id = product.id_product;
-        this.quantity = product.quantity;
-        this.img = img;
-    }
-
-    render() {
-        return `<div class="product-item" data-id="${this.id}">
-                    <img class="product-item__img" src="${this.img}" alt="photo product">
-                    <h3>${this.title}</h3>
-                    <p>${this.price}</p>
-                    <p>Количество = ${this.quantity}</p>
-                </div>`
-    }
-
-    // количество товара
-    quantityItem() {
-
-    }
-
-    // удалить товар из корзины
-    removeItem() {
-
-    }
-
-    // добавить товар в избранное
-    addToFavorite() {
-
-    }
-};
-
-class ProductsList {
-    constructor(container = '.products') {
-        this.container = container;
-        this.goods = [];
-        this._getProducts()
-            .then(data => {
-                this.goods = [...data];
-                this.render();
-            });
-    }
-
-    _getProducts() {
-        return fetch(`${urlAPI}/catalogData.json`)
-            .then(result => result.json())
-            .catch(error => console.log(error))
-    }
-
-    render() {
-        const block = document.querySelector('.products');
-        for (let product of this.goods) {
-            const productObj = new ProductItem(product);
-            block.insertAdjacentHTML('beforeend', productObj.render());
-        }
-    }
-
-    allPriceProducts() {
-        let sum = 0;
-        for (let i = 0; i < this.goods.length; i++) {
-            sum += this.goods[i].price;
-        };
-        return sum;
-    }
-};
-
-class ProductItem {
-    constructor(product, img = 'img/photo-product.jpg') {
-        this.title = product.product_name;
-        this.price = product.price;
-        this.id = product.id_product;
-        this.img = img;
-    }
-
-    render() {
-        return `<div class="product-item" data-id="${this.id}">
-                    <img class="product-item__img" src="${this.img}" alt="photo product">
-                    <h3>${this.title}</h3>
-                    <p>${this.price}</p>
-                    <button class="btn-cart" type="button">Купить</button>
-                </div>`
-    }
-};
-
-let list = new ProductsList();
-list.render();
-
-let listBasket = new Cart();
-listBasket.render();
-
-console.log(list.allPriceProducts());
+})
